3e58e17f95a0f2d1051816ba201ec1d2e3dd51e1
hodldeveloper <hodlcashtoken@gmail.com>
2024-11-10 15:53:12 +0000

    init images

--- src/config.js
+++ src/config.js
@@ -26,13 +26,10 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Labubu_Skin" },
+      { name: "Labubu_Hair" },
+      { name: "Labubu_Mouth" },
+      { name: "Labubu_Eyes" },
     ],
   },
 ];
